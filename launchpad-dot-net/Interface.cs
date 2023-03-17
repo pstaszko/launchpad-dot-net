7a1d1d6eaf44a4abd1bd812d57ca9b20a0a65e35
Paul Staszko <paulstaszko@gmail.com>
2023-03-16 19:22:45 -0500

    quick

--- launchpad-dot-net/Interface.cs
+++ launchpad-dot-net/Interface.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Midi;
 using Midi.Devices;
 using Midi.Enums;
 using Midi.Messages;
